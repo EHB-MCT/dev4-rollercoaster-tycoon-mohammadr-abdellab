98ba00a0e97a8719faf2acaaadfc4969409a553a
MohammadRashidEhb <mohammad.rashid@student.ehb.be>
2024-04-02 21:59:44 +0200

    Set up Spring Boot project structure
    This is the first commit: I created a Spring Boot project. It only includes the project structure; therefore, this is just a test commit to ensure that everything is okay and ready.

---
+++ RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/RollerCoasterTycoonApplication.kt
@@ -0,0 +1,11 @@
+package com.example.rollercoastertycoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RollerCoasterTycoonApplication
+
+fun main(args: Array<String>) {
+	runApplication<RollerCoasterTycoonApplication>(*args)
+}
