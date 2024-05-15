03b2e74d230879a15e83e5e850e9e9e8968e7a44
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-05-15 18:05:52 +0200

    creden

--- RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/model/Attraction.kt
+++ RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/model/Attraction.kt
@@ -1,6 +1,5 @@
 package com.example.rollercoastertycoon.model
 
-// Attraction.kt
 data class Attraction(
     val id: Double,
     val name: String,
@@ -10,6 +9,6 @@ data class Attraction(
     val image: String,
     val onRideVideo: String?,
     val operational: Boolean,
-    val maintenanceDates: List<String>, // Change this to a Date type if needed
-    val additionalAttributes: Map<String, Any> // Additional attributes as key-value pairs
+    val maintenanceDates: List<String>,
+    val additionalAttributes: Map<String, Any>
 )
