8a5d8ebce91d6c3304a9aec97161a35198a21489
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-05-16 22:13:47 +0200

    remove findbyid in repository

--- RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/repositories/UserRepository.kt
+++ RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/repositories/UserRepository.kt
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findById(User: Int): User
-}
+interface UserRepository : JpaRepository<User, Long>
 
