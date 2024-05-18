387a901fda0499da66bfd6b2c8a32ef2d8ec55da
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-05-18 21:34:01 +0200

    home

--- RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/controller/AttractionController.kt
+++ RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/controller/AttractionController.kt
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import com.example.rollercoastertycoon.dto.AttractionDTO
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/attractions")
 class AttractionController(private val attractionService: AttractionService) {
--- frontendView/attractionsInfoApp/src/App.vue
+++ frontendView/attractionsInfoApp/src/App.vue
@@ -1,47 +1,42 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <RouterLink class="navbar-brand" exact to="/">Attractions</RouterLink>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <RouterLink class="nav-link" exact to="/">Home</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" exact to="/items">Admin Dashboard</RouterLink>
+              </li>
+              <li class="nav-item">
+                <RouterLink class="nav-link" exact to="/loans">Favorites</RouterLink>
+              </li>
+            </ul>
+
+            <span style="color: white;" class="navbar-text ms-auto" v-if="loggedInUser">Welcome, {{ loggedInUser.name
+              }}</span>
+
+            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <button style="margin-left: 10px;" class="nav-link btn btn-primary" @click="logout">Login</button>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </nav>
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
