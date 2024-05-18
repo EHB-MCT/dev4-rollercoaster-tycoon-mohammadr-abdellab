dc59900cc17b07e8b3269d46a7dc91441452d05c
Abdella Boutaarourt <abdella.boutaarourt@student.ehb.be>
2024-05-18 23:57:25 +0200

    service category

--- RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/repositories/AttractionRepository.kt
+++ RollerCoasterTycoon/src/main/kotlin/com/example/rollercoastertycoon/repositories/AttractionRepository.kt
@@ -1,11 +1,13 @@
 package com.example.rollercoastertycoon.repositories
 
 import com.example.rollercoastertycoon.model.Attraction
+import com.example.rollercoastertycoon.model.Category
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AttractionRepository : JpaRepository<Attraction, Long>
-
+interface AttractionRepository : JpaRepository<Attraction, Long> {
+    fun existsByCategory(category: Category): Boolean
+}
 
 
